9918f50db52ed39ca061b475061c1319ef5fdcc2
andrew <andrew.brownbill@gmail.com>
2024-11-21 19:49:54 -0500

    Pre-commits working better.

--- software/rp2040_firmware/src/sound.rs
+++ software/rp2040_firmware/src/sound.rs
@@ -3,15 +3,50 @@
 use embassy_rp::interrupt;
 use embassy_rp::pwm::{Config, Pwm};
 use fixed::FixedU16;
+//#use core::include_bytes;
 
 const AUDIO_SIZE: usize = 1462987;
 const AUDIO: &[u8; AUDIO_SIZE] = include_bytes!("../assets/ode.bin");
 
-const BUFFER_SIZE: usize = 48000;
+pub struct Pipe<T: Copy + Default, const N: usize> {
+    buffer: [T; N],
+    read_idx: usize,
+    write_idx: usize,
+}
+
+impl<T: Copy + Default, const N: usize> Pipe<T, N> {
+    pub fn new() -> Self {
+        Self {
+            buffer: [T::default(); N],
+            read_idx: 0,
+            write_idx: 0,
+        }
+    }
+
+    pub fn add(&mut self, value: T) -> bool {
+        let next_write_idx = (self.write_idx + 1) % N;
+        if next_write_idx == self.read_idx {
+            // Hit the read head - return false as "unable to add"
+            return false;
+        }
+        self.buffer[self.write_idx] = value;
+        self.write_idx = next_write_idx;
+        return true;
+    }
+
+    pub fn get(&mut self) -> T {
+        if self.read_idx == self.write_idx {
+            T::default()
+        } else {
+            let rval = self.buffer[self.read_idx];
+            self.read_idx = (self.read_idx + 1) % N;
+            rval
+        }
+    }
+}
+
 const CONFIG_TOP: u16 = 512;
-static mut SOUND_PIPE: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
-static mut SOUND_PIPE_READ: usize = 0;
-static mut SOUND_PIPE_WRITE: usize = 0;
+static mut SOUND_PIPE: Option<Pipe<u8, 48000>> = None;
 static mut PWM_AB: Option<Pwm> = None;
 static mut PWM_CONFIG: Option<Config> = None;
 // target frequency is 48 khz
@@ -47,6 +82,7 @@ impl Sound {
             config.divider = FixedU16::from_bits(CLOCK_DIVIDER_U16);
             config.invert_b = true;
             PWM_AB.as_mut().unwrap().set_config(config);
+            SOUND_PIPE = Some(Pipe::new());
         }
 
         // Enable the interrupt for pwm slice 0
@@ -58,21 +94,16 @@ impl Sound {
         Self { audio_pos: 0 }
     }
 
-    pub fn update_one(&mut self) {
-        let value: u8 = AUDIO[self.audio_pos / 2];
-        self.audio_pos = (self.audio_pos + 1) % (AUDIO_SIZE * 2);
-        unsafe {
-            SOUND_PIPE[SOUND_PIPE_WRITE] = value;
-            SOUND_PIPE_WRITE = (SOUND_PIPE_WRITE + 1) % BUFFER_SIZE;
-        }
-    }
-
     pub fn update(&mut self) {
         unsafe {
-            let mut next_write: usize = (SOUND_PIPE_WRITE + 1) % BUFFER_SIZE;
-            while next_write != SOUND_PIPE_READ {
-                self.update_one();
-                next_write = (SOUND_PIPE_WRITE + 1) % BUFFER_SIZE;
+            let mut try_to_add_next: bool = true;
+            while try_to_add_next {
+                let value: u8 = AUDIO[self.audio_pos / 2];
+                let added = SOUND_PIPE.as_mut().unwrap().add(value);
+                if added {
+                    self.audio_pos = (self.audio_pos + 1) % (AUDIO_SIZE * 2);
+                }
+                try_to_add_next = added;
             }
         }
     }
@@ -81,14 +112,7 @@ impl Sound {
 #[interrupt]
 fn PWM_IRQ_WRAP() {
     unsafe {
-        let value: u8 = if SOUND_PIPE_READ == SOUND_PIPE_WRITE {
-            0
-        } else {
-            let rval = SOUND_PIPE[SOUND_PIPE_READ];
-            SOUND_PIPE_READ = (SOUND_PIPE_READ + 1) % BUFFER_SIZE;
-            rval
-        };
-
+        let value = SOUND_PIPE.as_mut().unwrap().get();
         let config = PWM_CONFIG.as_mut().unwrap();
         config.compare_a = value as u16;
         config.compare_b = value as u16;
