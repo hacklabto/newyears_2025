f1344404375866600e03be4e01fcd12846090dee
andrew <andrew.brownbill@gmail.com>
2024-11-26 13:23:48 -0500

    Some work simplifying the drawing of text.

--- software/rp2040_firmware/src/display.rs
+++ software/rp2040_firmware/src/display.rs
@@ -8,9 +8,19 @@ use ssd1306::prelude::I2CInterface;
 use ssd1306::rotation::DisplayRotation;
 use ssd1306::size::DisplaySize128x32;
 use ssd1306::Ssd1306;
+use embedded_graphics::mono_font::MonoTextStyle;
+use embedded_graphics::mono_font::ascii::FONT_9X18;
+use embedded_graphics::mono_font::ascii::FONT_9X18_BOLD;
+use embedded_graphics::text::TextStyleBuilder;
+use embedded_graphics::text::Alignment;
+use embedded_graphics::text::LineHeight;
+use embedded_graphics::text::Text;
+use embedded_graphics::prelude::Point;
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::Drawable;
 
 /// Turn the actual display class into something readable.
-pub type DisplaySSD<'a> = 
+pub type DisplaySSD<'a> =
     Ssd1306<
         I2CInterface<i2c::I2c<'a, I2C0, i2c::Blocking>>,
         DisplaySize128x32,
@@ -28,7 +38,7 @@ pub fn create_ssd_display<'a>(
     i2c0: embassy_rp::peripherals::I2C0,
     sclr: embassy_rp::peripherals::PIN_17,
     sda: embassy_rp::peripherals::PIN_16,
-) -> DisplaySSD<'a> 
+) -> DisplaySSD<'a>
 {
     let i2c = embassy_rp::i2c::I2c::new_blocking(
         i2c0,
@@ -43,9 +53,27 @@ pub fn create_ssd_display<'a>(
     display
 }
 
+pub fn draw_text<'a>( display: &mut DisplaySSD<'a>, text: &str, line: i32, bold: bool )
+{
+    // Create the character style.
+    let character_style = MonoTextStyle::new(
+        if bold { &FONT_9X18_BOLD } else {&FONT_9X18} , BinaryColor::On);
+
+    // Create a new text style.
+    let text_style = TextStyleBuilder::new()
+        .alignment(Alignment::Left)
+        .line_height(LineHeight::Percent(120))
+        .build();
+
+    let _ = Text::with_text_style(text,
+                Point::new(0, line * 16 + 15),
+                character_style,
+                text_style).draw( display );
+}
+
 // Some sample code for drawing on the display
 // TODO - this could just be a link to the github example I used.
- 
+
     /*
     pub fn drawing_reference(&mut self) {
         let yoffset = 8;
--- software/rp2040_firmware/src/main.rs
+++ software/rp2040_firmware/src/main.rs
@@ -1,14 +1,6 @@
 #![no_std]
 #![no_main]
 
-use embedded_graphics::mono_font::MonoTextStyle;
-use embedded_graphics::mono_font::ascii::FONT_10X20;
-use embedded_graphics::text::TextStyleBuilder;
-use embedded_graphics::text::Alignment;
-use embedded_graphics::text::LineHeight;
-use embedded_graphics::text::Text;
-use embedded_graphics::prelude::Point;
-use embedded_graphics::Drawable;
 use embedded_graphics::pixelcolor::BinaryColor;
 use heapless::String;
 use embedded_graphics::draw_target::DrawTarget;
@@ -24,7 +16,7 @@ async fn main(_spawner: embassy_executor::Spawner) {
         p.I2C0, p.PIN_17, // SCLR
         p.PIN_16, // SDA
     );
-    let mut animating_gif = hackernewyears::AnimatingGif::new();
+    let mut _animating_gif = hackernewyears::AnimatingGif::new();
 
     let mut leds = hackernewyears::LEDs::new(p.PIN_11, p.PIN_12, p.PIN_13);
     let buttons = hackernewyears::Buttons::new(p.PIN_2, p.PIN_3, p.PIN_4, p.PIN_5 );
@@ -43,33 +35,23 @@ async fn main(_spawner: embassy_executor::Spawner) {
         //animating_gif.update( &mut display );
         let time = hackernewyears::Timer::ms_from_start() as u32;
 
-        // Create a new character style.
-        let character_style = MonoTextStyle::new(&FONT_10X20, BinaryColor::On);
+        let time_as_str: String<100> = String::try_from(time).unwrap();
 
-        // Create a new text style.
-        let text_style = TextStyleBuilder::new()
-                .alignment(Alignment::Center)
-                .line_height(LineHeight::Percent(150))
-                .build();
-
-        let mut time_as_str: String<100> = String::try_from(time).unwrap();
-        time_as_str.push(' ').unwrap();
+        let mut button_status : String<100> = Default::default();
         if buttons.is_pressed(Button::B0) {
-            time_as_str.push('A').unwrap();
+            button_status.push('A').unwrap();
         }
         if buttons.is_pressed(Button::B1) {
-            time_as_str.push('B').unwrap();
+            button_status.push('B').unwrap();
         }
         if buttons.is_pressed(Button::B2) {
-            time_as_str.push('C').unwrap();
+            button_status.push('C').unwrap();
         }
         if buttons.is_pressed(Button::B3) {
-            time_as_str.push('D').unwrap();
+            button_status.push('D').unwrap();
         }
-        let _ = Text::with_text_style(&time_as_str,
-                Point::new(64,15),
-                character_style,
-                text_style).draw( &mut display );
+        hackernewyears::display::draw_text(&mut display, time_as_str.as_str(), 0 , true );
+        hackernewyears::display::draw_text(&mut display, button_status.as_str(), 1, false );
         leds.update();
         gsound.update();
         ticker.next().await;
