3a6a07434448c5338302af3bad13334bc3b48707
Anrew Brownbill <andrew.brownbill@gmail.com>
2025-06-18 19:18:17 +0200

    Remove consts from sound source pool, which effectively create unique types, to make things vtable compatible

--- software/midi-nostd/src/sound_source.rs
+++ software/midi-nostd/src/sound_source.rs
@@ -115,33 +115,31 @@ pub trait SoundSource<SAMPLE: SoundSample, const PLAY_FREQUENCY: u32> {
 
 pub trait SoundSourcePool<SAMPLE: SoundSample, const PLAY_FREQUENCY: u32>
 {
-    const TYPE: usize;
-    const POOL_SIZE: usize;
-
     fn pool_alloc(self: &mut Self) -> usize;
     fn pool_has_next(self: &Self, element: usize) -> bool;
     fn pool_get_next(self: &mut Self, element: usize) -> SAMPLE;
+    fn get_type_id(self: &Self) -> usize;
 
     fn alloc(self: &mut Self) -> SoundSourceId {
         let pool_id = self.pool_alloc();
-        SoundSourceId::new(SoundSourceType::from_usize(Self::TYPE), pool_id )
+        SoundSourceId::new(SoundSourceType::from_usize(self.get_type_id()), pool_id )
     }
 
     fn has_next(self: &mut Self, id: &SoundSourceId ) -> bool {
-        assert_eq!( Self::TYPE, id.source_type as usize);
+        assert_eq!( self.get_type_id(), id.source_type as usize);
         self.pool_has_next( id.id )
     }
 
     fn get_next(self: &mut Self, id: &SoundSourceId ) -> SAMPLE {
-        assert_eq!( Self::TYPE, id.source_type as usize);
+        assert_eq!( self.get_type_id(), id.source_type as usize);
         self.pool_get_next( id.id )
     }
 }
 
 
-//pub struct SoundSources<SAMPLE: SoundSample, const PLAY_FREQUENCY: u32>
-//{   
-//    test: &mut dyn SoundSourcePool<SAMPLE, PLAY_FREQUENCY>
-//}
+pub struct SoundSources<'a, SAMPLE: SoundSample, const PLAY_FREQUENCY: u32>
+{   
+    test: &'a mut dyn SoundSourcePool<SAMPLE, PLAY_FREQUENCY>
+}
 
 
