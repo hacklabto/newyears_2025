525aa935f0160b653b14517d24d75f1b53f9d24b
Anrew Brownbill <andrew.brownbill@gmail.com>
2025-06-26 15:41:58 +0200

    Refactor

---
+++ software/midi-nostd/src/free_list.rs
@@ -0,0 +1,61 @@
+#[allow(unused)]
+pub trait SoundSourceFreeList {
+    fn alloc(self: &mut Self) -> usize;
+    fn free(self: &mut Self, item_to_free: usize);
+}
+
+#[allow(unused)]
+pub struct SoundSourceFreeListImpl<const N: usize> {
+    free_list: [Option<usize>; N],
+    free_list_head: Option<usize>,
+}
+
+#[allow(unused)]
+impl<const N: usize> SoundSourceFreeList for SoundSourceFreeListImpl<N> {
+    fn alloc(self: &mut Self) -> usize {
+        let allocated_item = self
+            .free_list_head
+            .expect("Unhandled out of sound pool error");
+        self.free_list_head = self.free_list[allocated_item];
+        self.free_list[allocated_item] = None;
+        allocated_item
+    }
+    fn free(self: &mut Self, item_to_free: usize) {
+        assert!(self.free_list[item_to_free].is_none());
+        self.free_list[item_to_free] = self.free_list_head;
+        self.free_list_head = Some(item_to_free);
+    }
+}
+
+#[allow(unused)]
+impl<const N: usize> SoundSourceFreeListImpl<N> {
+    pub fn new() -> Self {
+        let free_list: [Option<usize>; N] =
+            core::array::from_fn(|idx| if idx == N - 1 { None } else { Some(idx + 1) });
+        let free_list_head: Option<usize> = Some(0);
+
+        Self {
+            free_list,
+            free_list_head,
+        }
+    }
+}
+
+#[cfg(test)]
+mod free_list_tests {
+    use crate::free_list::*;
+    #[test]
+    fn free_list_should_alloc_and_free() {
+        let mut free_list: SoundSourceFreeListImpl<3> = SoundSourceFreeListImpl::new();
+        assert_eq!(0, free_list.alloc());
+        assert_eq!(1, free_list.alloc());
+        assert_eq!(2, free_list.alloc());
+        free_list.free(1);
+        free_list.free(0);
+        free_list.free(2);
+        assert_eq!(2, free_list.alloc());
+        assert_eq!(0, free_list.alloc());
+        assert_eq!(1, free_list.alloc());
+    }
+}
+
--- software/midi-nostd/src/lib.rs
+++ software/midi-nostd/src/lib.rs
@@ -5,3 +5,5 @@ mod sound_sample;
 mod sound_source;
 mod sound_sources;
 mod wave_generator;
+mod free_list;
+
