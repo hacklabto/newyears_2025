0476f9dd1434b431e5e7cd7b26a8e2538e5e2f9c
andrew <andrew.brownbill@gmail.com>
2025-07-19 14:45:01 -0400

    Code for tracks is more functional.

--- software/midi-nostd/src/midi.rs
+++ software/midi-nostd/src/midi.rs
@@ -9,20 +9,42 @@ use crate::sound_source_msgs::SoundSourceMsgs;
 //use crate::sound_source_msgs::SoundSourceValue;
 use crate::sound_sources::SoundSources;
 use core::marker::PhantomData;
-use core::slice;
+//use core::slice;
 use midly::Smf;
 
 #[allow(unused)]
-pub struct MidiTrack<'a, T: SoundSample, const PLAY_FREQUENCY: u32> {
+pub struct MidiTrack<T: SoundSample, const PLAY_FREQUENCY: u32> {
     time: u32,
     next_event: u32,
-    track_iter: slice::Iter<'a, midly::TrackEvent<'a>>,
-    current_event: Option<&'a midly::TrackEvent<'a>>,
+    next_idx: usize,
+    //track_iter: slice::Iter<'a, midly::TrackEvent<'a>>,
+    //current_event: Option<&'a midly::TrackEvent<'a>>,
     _marker: PhantomData<T>,
 }
 
-impl<'a, T: SoundSample, const PLAY_FREQUENCY: u32> MidiTrack<'a, T, PLAY_FREQUENCY> {
-    pub fn new(track: &'a midly::Track<'a>) -> Self {
+impl<T: SoundSample, const PLAY_FREQUENCY: u32> MidiTrack<T, PLAY_FREQUENCY> {
+    pub fn new() -> Self {
+        let time: u32 = 0;
+        let next_event: u32 = 0;
+        let next_idx: usize = 0;
+        Self {
+            time,
+            next_event,
+            next_idx,
+            _marker: PhantomData {},
+        }
+    }
+
+    pub fn update<'a>(
+        self: &mut Self,
+        events: &'a midly::Track<'a>,
+        _new_msgs: &mut SoundSourceMsgs,
+    ) {
+        let _current_event = events[self.next_idx];
+    }
+
+    /*
+    pub fn new(track: midly::Track) -> Self {
         let time: u32 = 0;
         let mut track_iter = track.iter();
         let current_event = track_iter.next();
@@ -39,35 +61,23 @@ impl<'a, T: SoundSample, const PLAY_FREQUENCY: u32> MidiTrack<'a, T, PLAY_FREQUE
             current_event,
             _marker: PhantomData {},
         }
-    }
+    }*/
 }
 
-///
-/// Midi Playback
-///
-#[allow(unused)]
-pub struct Midi<'a, T: SoundSample, const PLAY_FREQUENCY: u32> {
-    smf: Option<Smf<'a>>,
+pub struct MidiReal<'a, T: SoundSample, const PLAY_FREQUENCY: u32> {
+    smf: Smf<'a>,
+    track: MidiTrack<T, PLAY_FREQUENCY>,
     _marker: PhantomData<T>,
 }
 
-impl<T: SoundSample, const PLAY_FREQUENCY: u32> Default for Midi<'_, T, PLAY_FREQUENCY> {
-    fn default() -> Self {
-        Self {
-            smf: None,
-            _marker: PhantomData {},
-        }
-    }
-}
-
-impl<T: SoundSample, const PLAY_FREQUENCY: u32> Midi<'_, T, PLAY_FREQUENCY> {
+impl<T: SoundSample, const PLAY_FREQUENCY: u32> MidiReal<'_, T, PLAY_FREQUENCY> {
     pub fn new(midi_bytes: &'static [u8]) -> Self {
-        let smf = Some(
-            midly::Smf::parse(midi_bytes)
-                .expect("It's inlined data, so it better work, gosh darn it"),
-        );
+        let smf = midly::Smf::parse(midi_bytes)
+            .expect("It's inlined data, so it better work, gosh darn it");
+        let track = MidiTrack::new();
         Self {
             smf,
+            track,
             _marker: PhantomData {},
         }
     }
@@ -75,7 +85,7 @@ impl<T: SoundSample, const PLAY_FREQUENCY: u32> Midi<'_, T, PLAY_FREQUENCY> {
 
 #[allow(unused)]
 impl<'a, T: SoundSample, const PLAY_FREQUENCY: u32> SoundSource<'a, T, PLAY_FREQUENCY>
-    for Midi<'a, T, PLAY_FREQUENCY>
+    for MidiReal<'a, T, PLAY_FREQUENCY>
 {
     fn get_next(self: &Self, all_sources: &dyn SoundSources<T, PLAY_FREQUENCY>) -> T {
         T::max()
@@ -85,9 +95,54 @@ impl<'a, T: SoundSample, const PLAY_FREQUENCY: u32> SoundSource<'a, T, PLAY_FREQ
         true
     }
 
-    fn update(&mut self, _new_msgs: &mut SoundSourceMsgs) {}
+    fn update(&mut self, new_msgs: &mut SoundSourceMsgs) {
+        self.track.update(&self.smf.tracks[0], new_msgs)
+    }
 
     fn handle_msg(&mut self, msg: &SoundSourceMsg, new_msgs: &mut SoundSourceMsgs) {}
+}
+
+///
+/// Midi Playback
+///
+#[allow(unused)]
+pub struct Midi<'a, T: SoundSample, const PLAY_FREQUENCY: u32> {
+    midi_maybe: Option<MidiReal<'a, T, PLAY_FREQUENCY>>,
+}
+
+impl<T: SoundSample, const PLAY_FREQUENCY: u32> Default for Midi<'_, T, PLAY_FREQUENCY> {
+    fn default() -> Self {
+        Self { midi_maybe: None }
+    }
+}
+
+impl<T: SoundSample, const PLAY_FREQUENCY: u32> Midi<'_, T, PLAY_FREQUENCY> {
+    pub fn new(midi_bytes: &'static [u8]) -> Self {
+        Self {
+            midi_maybe: Some(MidiReal::new(midi_bytes)),
+        }
+    }
+}
+
+#[allow(unused)]
+impl<'a, T: SoundSample, const PLAY_FREQUENCY: u32> SoundSource<'a, T, PLAY_FREQUENCY>
+    for Midi<'a, T, PLAY_FREQUENCY>
+{
+    fn get_next(self: &Self, all_sources: &dyn SoundSources<T, PLAY_FREQUENCY>) -> T {
+        self.midi_maybe.as_ref().unwrap().get_next(all_sources)
+    }
+
+    fn has_next(self: &Self, all_sources: &dyn SoundSources<T, PLAY_FREQUENCY>) -> bool {
+        self.midi_maybe.as_ref().unwrap().has_next(all_sources)
+    }
+
+    fn update(&mut self, new_msgs: &mut SoundSourceMsgs) {
+        self.midi_maybe.as_mut().unwrap().update(new_msgs)
+    }
+
+    fn handle_msg(&mut self, msg: &SoundSourceMsg, new_msgs: &mut SoundSourceMsgs) {
+        self.midi_maybe.as_mut().unwrap().handle_msg(msg, new_msgs);
+    }
 }
 
 /*
