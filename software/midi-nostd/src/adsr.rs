8fe3d540efffdaeaad42bc189c524578381726ad
Andrew Brownbill <andrew.brownbill@gmail.com>
2025-07-23 21:51:13 -0400

    Remove adsr tests from core functionalty

--- software/midi-nostd/src/adsr.rs
+++ software/midi-nostd/src/adsr.rs
@@ -1,9 +1,7 @@
 use crate::sound_sample::SoundSample;
-use crate::sound_sample::SoundSampleI32;
 use crate::sound_sample::SoundScale;
 use crate::sound_source::SoundSource;
 use crate::sound_source_core::SoundSourceCore;
-use crate::sound_source_id::SoundSourceId;
 use crate::sound_source_msgs::SoundSourceAdsrInit;
 use crate::sound_source_msgs::SoundSourceMsg;
 use crate::sound_source_msgs::SoundSourceMsgs;
@@ -205,95 +203,65 @@ impl<T: SoundSample, const PLAY_FREQUENCY: u32> SoundSource<'_, T, PLAY_FREQUENC
     }
 }
 
-pub fn create_adsr(
-    all_pools: &mut dyn SoundSources<SoundSampleI32, 24000>,
-    init_values: SoundSourceAdsrInit,
-) -> SoundSourceId {
-    let mut msgs = SoundSourceMsgs::default();
-    msgs.append(SoundSourceMsg::new(
-        SoundSourceId::get_top_id(),
-        SoundSourceId::get_top_id(),
-        SoundSourceValue::AdsrInit { init_values },
-    ));
-    all_pools.process_and_clear_msgs(&mut msgs);
-
-    all_pools
-        .get_last_created_sound_source()
-        .expect("Id should have been recorded")
-        .clone()
-}
-
 #[cfg(test)]
 mod tests {
     use crate::adsr::*;
-    use crate::sound_sources::SoundSources;
-    use crate::sound_sources_impl::SoundSourcesImpl;
+    use crate::sound_sample::SoundSampleI32;
 
     #[test]
     fn basic_adsr_test() {
-        let mut all_pools = SoundSourcesImpl::<SoundSampleI32, 24000, 3, 3, 3>::default();
-        let adsr_id = create_adsr(
-            &mut all_pools,
-            SoundSourceAdsrInit::new(
-                SoundScale::new_percent(100),
-                SoundScale::new_percent(50),
-                2,
-                4,
-                4,
-            ),
+        let adsr_init = SoundSourceAdsrInit::new(
+            SoundScale::new_percent(100),
+            SoundScale::new_percent(50),
+            2,
+            4,
+            4,
         );
 
-        let mut new_msgs = SoundSourceMsgs::default();
+        let mut adsr = CoreAdsr::<SoundSampleI32, 24000>::default();
+        adsr.init(&adsr_init);
 
         // Attack state, 2 ticks to get to attack volume (max) from 0
-        assert_eq!(0x8000, all_pools.get_next(&adsr_id).to_u16());
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0xbfff, all_pools.get_next(&adsr_id).to_u16());
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0xffff, all_pools.get_next(&adsr_id).to_u16());
+        assert_eq!(0x8000, adsr.get_next().to_u16());
+        adsr.update();
+        assert_eq!(0xbfff, adsr.get_next().to_u16());
+        adsr.update();
+        assert_eq!(0xffff, adsr.get_next().to_u16());
 
         // Delay state, 4 ticks to get to Sustain Volume (50%) from attack volume
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0xeffe, all_pools.get_next(&adsr_id).to_u16());
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0xdffe, all_pools.get_next(&adsr_id).to_u16());
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0xcffe, all_pools.get_next(&adsr_id).to_u16());
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0xbfff, all_pools.get_next(&adsr_id).to_u16());
+        adsr.update();
+        assert_eq!(0xeffe, adsr.get_next().to_u16());
+        adsr.update();
+        assert_eq!(0xdffe, adsr.get_next().to_u16());
+        adsr.update();
+        assert_eq!(0xcffe, adsr.get_next().to_u16());
+        adsr.update();
+        assert_eq!(0xbfff, adsr.get_next().to_u16());
 
         // Sustain state
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0xbfff, all_pools.get_next(&adsr_id).to_u16());
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0xbfff, all_pools.get_next(&adsr_id).to_u16());
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0xbfff, all_pools.get_next(&adsr_id).to_u16());
+        adsr.update();
+        assert_eq!(0xbfff, adsr.get_next().to_u16());
+        adsr.update();
+        assert_eq!(0xbfff, adsr.get_next().to_u16());
+        adsr.update();
+        assert_eq!(0xbfff, adsr.get_next().to_u16());
 
-        let mut msgs = SoundSourceMsgs::default();
-        msgs.append(SoundSourceMsg::new(
-            adsr_id.clone(),
-            SoundSourceId::get_top_id(),
-            SoundSourceValue::ReleaseAdsr,
-        ));
-        all_pools.process_and_clear_msgs(&mut msgs);
+        adsr.trigger_release();
 
         // Release state, 4 ticks to get to quiet from Sustain Volume
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0xafff, all_pools.get_next(&adsr_id).to_u16());
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0x9fff, all_pools.get_next(&adsr_id).to_u16());
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0x8fff, all_pools.get_next(&adsr_id).to_u16());
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0x8000, all_pools.get_next(&adsr_id).to_u16());
-        assert_eq!(true, all_pools.has_next(&adsr_id));
+        adsr.update();
+        assert_eq!(0xafff, adsr.get_next().to_u16());
+        adsr.update();
+        assert_eq!(0x9fff, adsr.get_next().to_u16());
+        adsr.update();
+        assert_eq!(0x8fff, adsr.get_next().to_u16());
+        adsr.update();
+        assert_eq!(0x8000, adsr.get_next().to_u16());
+        assert_eq!(true, adsr.has_next());
 
         // End state.  Report silence and no more data
-        all_pools.update(&mut new_msgs);
-        assert_eq!(0x8000, all_pools.get_next(&adsr_id).to_u16());
-        assert_eq!(false, all_pools.has_next(&adsr_id));
-
-        all_pools.free(adsr_id);
+        adsr.update();
+        assert_eq!(0x8000, adsr.get_next().to_u16());
+        assert_eq!(false, adsr.has_next());
     }
 }
