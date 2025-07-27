6833e190325570e1b51be2e2988e6c9bf7ffa186
andrew <andrew.brownbill@gmail.com>
2025-07-27 16:17:09 -0400

    adsr fixed.

--- software/midi-nostd/src/adsr.rs
+++ software/midi-nostd/src/adsr.rs
@@ -1,4 +1,5 @@
 use crate::sound_sample::time_to_ticks;
+//use crate::sound_sample::I32Fraction;
 use crate::sound_sample::SoundSampleI32;
 use crate::sound_source_core::SoundSourceCore;
 
@@ -11,11 +12,19 @@ impl SoundSourceAdsrInit {
     }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub enum AdsrState {
-    Attack,
-    Delay,
+/*
+#[derive(PartialEq, Eq, Debug)]
+pub enum AdsrState<const A: i32, const D: i32, const R: i32> {
+    Attack { value: I32Fraction<A>},
+    Delay { value: I32Fraction<D> },
     Sustain,
+    Release { value: I32Fraction<R> },
+    Ended,
+}
+*/
+#[derive(PartialEq, Eq, Debug)]
+pub enum AdsrState {
+    ADS,
     Release,
     Ended,
 }
@@ -30,7 +39,6 @@ pub struct CoreAdsr<
     const SUSTAIN_VOLUME: u8,
     const R: i32,
 > {
-    state: AdsrState,            // CurrentState
     time_since_state_start: i32, // units are 1/PLAY_FREQUENCY
 }
 
@@ -49,26 +57,10 @@ impl<
     const D_TICKS: i32 = time_to_ticks::<PLAY_FREQUENCY>(D);
     const R_TICKS: i32 = time_to_ticks::<PLAY_FREQUENCY>(R);
 
-    fn update_internal(&mut self) {
-        if self.state == AdsrState::Attack {
-            if self.time_since_state_start >= Self::A_TICKS {
-                self.time_since_state_start = 0;
-                self.state = AdsrState::Delay;
-            }
-        }
-        if self.state == AdsrState::Delay {
-            if self.time_since_state_start >= Self::D_TICKS {
-                self.time_since_state_start = 0;
-                self.state = AdsrState::Sustain;
-            }
-        }
-        if self.state == AdsrState::Release {
-            if self.time_since_state_start > Self::R_TICKS {
-                self.time_since_state_start = 0;
-                self.state = AdsrState::Ended;
-            }
-        }
-    }
+    const A_END: i32 = Self::A_TICKS;
+    const D_END: i32 = Self::A_END + Self::D_TICKS;
+    const R_START: i32 = time_to_ticks::<PLAY_FREQUENCY>(10000);
+    const R_END: i32 = Self::R_START + Self::R_TICKS;
 }
 
 impl<
@@ -82,41 +74,36 @@ impl<
     type InitValuesType = SoundSourceAdsrInit;
 
     fn init(self: &mut Self, _init_values: &Self::InitValuesType) {
-        self.state = AdsrState::Attack;
         self.time_since_state_start = 0;
-        self.update_internal();
     }
 
     fn get_next(self: &mut Self) -> SoundSampleI32 {
-        let scale: SoundSampleI32 = match self.state {
-            AdsrState::Attack => Self::ATTACK_VOLUME_SCALE
-                .mul_by_fraction(self.time_since_state_start, Self::A_TICKS),
-            AdsrState::Delay => {
-                Self::ATTACK_VOLUME_SCALE
-                    .mul_by_fraction(Self::D_TICKS - self.time_since_state_start, Self::D_TICKS)
-                    + Self::SUSTAIN_VOLUME_SCALE
-                        .mul_by_fraction(self.time_since_state_start, Self::D_TICKS)
-            }
-            AdsrState::Sustain => Self::SUSTAIN_VOLUME_SCALE,
-            AdsrState::Release => Self::SUSTAIN_VOLUME_SCALE
-                .mul_by_fraction(Self::R_TICKS - self.time_since_state_start, Self::R_TICKS),
-            AdsrState::Ended => SoundSampleI32::ZERO,
+        let scale: SoundSampleI32 = if self.time_since_state_start < Self::A_END {
+            Self::ATTACK_VOLUME_SCALE.mul_by_fraction(self.time_since_state_start, Self::A_TICKS)
+        } else if self.time_since_state_start < Self::D_END {
+            let time_since_d_start = self.time_since_state_start - Self::A_TICKS;
+            Self::ATTACK_VOLUME_SCALE
+                .mul_by_fraction(Self::D_TICKS - time_since_d_start, Self::D_TICKS)
+                + Self::SUSTAIN_VOLUME_SCALE.mul_by_fraction(time_since_d_start, Self::D_TICKS)
+        } else if self.time_since_state_start < Self::R_START {
+            Self::SUSTAIN_VOLUME_SCALE
+        } else if self.time_since_state_start <= Self::R_END {
+            let time_since_r_start = self.time_since_state_start - Self::R_START;
+            Self::SUSTAIN_VOLUME_SCALE
+                .mul_by_fraction(Self::R_TICKS - time_since_r_start, Self::R_TICKS)
+        } else {
+            SoundSampleI32::ZERO
         };
         self.time_since_state_start = self.time_since_state_start + 1;
-        self.update_internal();
-
         scale
     }
 
     fn has_next(self: &Self) -> bool {
-        self.state != AdsrState::Ended
+        self.time_since_state_start <= Self::R_END
     }
 
     fn trigger_note_off(self: &mut Self) {
-        // TODO, What if we aren't in sustain?  Probably I should take
-        // the current volume and run the release on that.
-        self.state = AdsrState::Release;
-        self.time_since_state_start = 0;
+        self.time_since_state_start = Self::R_START;
     }
 }
 
@@ -129,11 +116,9 @@ impl<
     > Default for CoreAdsr<PLAY_FREQUENCY, A, D, SUSTAIN_VOLUME, R>
 {
     fn default() -> Self {
-        let state = AdsrState::Ended;
         let time_since_state_start = 0;
 
         Self {
-            state,
             time_since_state_start,
         }
     }
@@ -147,21 +132,24 @@ mod tests {
     fn basic_adsr_test() {
         let adsr_init = SoundSourceAdsrInit::new();
 
-        let mut adsr = CoreAdsr::<1000, 2, 4, 50, 4>::default();
+        let mut adsr = CoreAdsr::<1000, 2, 4, 50, 8>::default();
         adsr.init(&adsr_init);
 
         // Attack state, 2 ticks to get to attack volume (max) from 0
+        assert_eq!(true, adsr.has_next());
         assert_eq!(0x0000, adsr.get_next().to_i32());
         assert_eq!(0x4000, adsr.get_next().to_i32());
         assert_eq!(0x8000, adsr.get_next().to_i32());
 
         // Delay state, 4 ticks to get to Sustain Volume (50%) from attack volume
+        assert_eq!(true, adsr.has_next());
         assert_eq!(0x7000, adsr.get_next().to_i32());
         assert_eq!(0x6000, adsr.get_next().to_i32());
         assert_eq!(0x5000, adsr.get_next().to_i32());
         assert_eq!(0x4000, adsr.get_next().to_i32());
 
         // Sustain state
+        assert_eq!(true, adsr.has_next());
         assert_eq!(0x4000, adsr.get_next().to_i32());
         assert_eq!(0x4000, adsr.get_next().to_i32());
         assert_eq!(0x4000, adsr.get_next().to_i32());
@@ -169,14 +157,24 @@ mod tests {
         assert_eq!(0x4000, adsr.get_next().to_i32());
 
         // Release state, 4 ticks to get to quiet from Sustain Volume
+        assert_eq!(true, adsr.has_next());
+        assert_eq!(0x3800, adsr.get_next().to_i32());
         assert_eq!(0x3000, adsr.get_next().to_i32());
+        assert_eq!(0x2800, adsr.get_next().to_i32());
         assert_eq!(0x2000, adsr.get_next().to_i32());
+        assert_eq!(0x1800, adsr.get_next().to_i32());
         assert_eq!(0x1000, adsr.get_next().to_i32());
+        assert_eq!(0x0800, adsr.get_next().to_i32());
         assert_eq!(true, adsr.has_next());
         assert_eq!(0x0000, adsr.get_next().to_i32());
 
         // End state.  Report silence and no more data
         assert_eq!(false, adsr.has_next());
+        assert_eq!(0x0000, adsr.get_next().to_i32());
+        assert_eq!(0x0000, adsr.get_next().to_i32());
+        assert_eq!(0x0000, adsr.get_next().to_i32());
+        assert_eq!(0x0000, adsr.get_next().to_i32());
+        assert_eq!(0x0000, adsr.get_next().to_i32());
         assert_eq!(0x0000, adsr.get_next().to_i32());
         assert_eq!(false, adsr.has_next());
     }
