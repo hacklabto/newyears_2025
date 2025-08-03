c5ce81a2f2d6d4270640ff107ffd67d8a339c670
Andrew Brownbill <andrew.brownbill@gmail.com>
2025-08-03 18:32:37 -0400

    Reduce CPU usage

--- software/example-midi/entertainer/src/main.rs
+++ software/example-midi/entertainer/src/main.rs
@@ -27,7 +27,7 @@ fn run() -> Result<(), pa::Error> {
     let (header, tracks) = midly::parse(include_bytes!("../assets/entertainer.mid"))
         .expect("It's inlined data, so its expected to parse");
 
-    type MyMidi<'a> = Midi<'a, 24000, 24000, 64, 16>;
+    type MyMidi<'a> = Midi<'a, 24000, 240, 64, 16>;
     println!(
         "Midi structure is currently using {} bytes",
         std::mem::size_of::<MyMidi>()
