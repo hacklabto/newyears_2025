397fa25dd6f806a6b6ff8b509c4196de1613aae2
Andrew Brownbill <andrew.brownbill@gmail.com>
2025-08-04 12:06:46 -0400

    Commite LFO template and use it for cello.

--- software/midi-nostd/src/cello.rs
+++ software/midi-nostd/src/cello.rs
@@ -1,84 +1,38 @@
-use crate::adsr::CoreAdsr;
-use crate::double_oscillator::DoubleOscillator;
-use crate::filter::Filter;
-use crate::lfo_amplitude::LfoAmplitude;
+use crate::instrument_template_amp_lfo::InstrumentTemplateAmpLfo;
+use crate::instrument_template_basic::FrequencyCalculator;
 use crate::midi_notes::midi_note_to_freq;
 use crate::midi_notes::FREQUENCY_MULTIPLIER;
 use crate::note::SoundSourceNoteInit;
-use crate::oscillator::CoreOscillator;
 use crate::oscillator::OscillatorType;
-use crate::sound_sample::SoundSampleI32;
-use crate::sound_source_core::SoundSourceCore;
 
-type CelloOscillatorPair<const P_FREQ: u32, const U_FREQ: u32> = DoubleOscillator<
-    P_FREQ,
-    U_FREQ,
-    CoreOscillator<P_FREQ, U_FREQ, 10, 100, { OscillatorType::PulseWidth as usize }>,
-    CoreOscillator<P_FREQ, U_FREQ, 50, 100, { OscillatorType::PulseWidth as usize }>,
-    false,
->;
+pub struct CelloLowPassCalculator {}
 
-type CelloOscillatorLfo<const P_FREQ: u32, const U_FREQ: u32> = LfoAmplitude<
-    P_FREQ,
-    U_FREQ,
-    CelloOscillatorPair<P_FREQ, U_FREQ>,
-    { OscillatorType::Sine as usize },
-    { 15 * FREQUENCY_MULTIPLIER / 2 },
-    5,
->;
-
-type CelloOscillatorAdsr<const P_FREQ: u32, const U_FREQ: u32> =
-    CoreAdsr<P_FREQ, U_FREQ, 006, 5000, 100, 300, CelloOscillatorLfo<P_FREQ, U_FREQ>>;
-
-type CelloFiltered<const P_FREQ: u32, const U_FREQ: u32> =
-    Filter<P_FREQ, U_FREQ, CelloOscillatorAdsr<P_FREQ, U_FREQ>>;
-
-///
-/// Cello.  Now sort of a proof of concept.
-///
-pub struct Cello<const P_FREQ: u32, const U_FREQ: u32> {
-    core: CelloFiltered<P_FREQ, U_FREQ>,
-}
-
-impl<const P_FREQ: u32, const U_FREQ: u32> SoundSourceCore<P_FREQ, U_FREQ>
-    for Cello<P_FREQ, U_FREQ>
-{
-    type InitValuesType = SoundSourceNoteInit;
-
-    fn get_next(self: &mut Self) -> SoundSampleI32 {
-        self.core.get_next()
-    }
-
-    fn update(self: &mut Self) {
-        self.core.update()
-    }
-
-    fn has_next(self: &Self) -> bool {
-        self.core.has_next()
-    }
-
-    fn new(init_values: Self::InitValuesType) -> Self {
+impl FrequencyCalculator for CelloLowPassCalculator {
+    fn get_cutoff_frequency(init_values: &SoundSourceNoteInit) -> u32 {
         let frequency_1 = midi_note_to_freq(init_values.key);
-        let frequency_2 = midi_note_to_freq(init_values.key);
-        let adsr_init = (init_values.velocity as i32) << 8;
-
-        // Basically just made this stuff up.
-        //let cutoff_frequency =
-        //200 + ((init_values.key as u32) * 5) + ((init_values.velocity as u32) / 5);
-        let cutoff_frequency = ((frequency_1 / FREQUENCY_MULTIPLIER) * 90 / 100) + 400;
-
-        let core = CelloFiltered::<P_FREQ, U_FREQ>::new((
-            ((frequency_1, frequency_2), adsr_init),
-            cutoff_frequency,
-        ));
-        return Self { core };
-    }
-
-    fn trigger_note_off(self: &mut Self) {
-        self.core.trigger_note_off();
-    }
-
-    fn restart(self: &mut Self, vel: u8) {
-        self.core.restart(vel);
+        ((frequency_1 / FREQUENCY_MULTIPLIER) * 90 / 100) + 400
     }
 }
+
+pub type Cello<const P_FREQ: u32, const U_FREQ: u32> = InstrumentTemplateAmpLfo<
+    P_FREQ,
+    U_FREQ,
+    10,                                      // Oscillator 0 pulse width
+    100,                                     // Oscillator 0 volume
+    { OscillatorType::PulseWidth as usize }, // Oscillator 0 wave form
+    0,                                       // Oscillator 0 note offset
+    50,                                      // Oscillator 1 pulse width
+    100,                                     // Oscillator 1 volume
+    { OscillatorType::PulseWidth as usize }, // Oscillator 1 wave form
+    0,                                       // Oscillator 1 note offset
+    true,                                    // Sync Oscillator 1 to 0
+    { OscillatorType::Sine as usize },       // LFO wave form
+    { 15 * FREQUENCY_MULTIPLIER / 2 },       // LFO frequency (7.5 hz)
+    5,                                       // LFO Depth
+    6,                                       // A
+    5000,                                    // D
+    100,                                     // S
+    300,                                     // R
+    CelloLowPassCalculator,
+>;
+
