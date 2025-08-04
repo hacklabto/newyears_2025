f41aa052bc4f09da9f0c6f7e4d176d59cffc774d
Andrew Brownbill <andrew.brownbill@gmail.com>
2025-08-04 15:01:55 -0400

    I wouldn't say it's a good french horn, but it is a french horn.

--- software/midi-nostd/src/french_horn.rs
+++ software/midi-nostd/src/french_horn.rs
@@ -1,54 +1,26 @@
-use crate::adsr::CoreAdsr;
-use crate::filter::Filter;
-use crate::midi_notes::midi_note_to_freq;
-use crate::note::SoundSourceNoteInit;
-use crate::oscillator::CoreOscillator;
+use crate::instrument_low_pass_filters::GenericLowPassCalculator;
+use crate::instrument_template_amp_lfo::InstrumentTemplateAmpLfo;
+use crate::midi_notes::FREQUENCY_MULTIPLIER;
 use crate::oscillator::OscillatorType;
-use crate::sound_sample::SoundSampleI32;
-use crate::sound_source_core::SoundSourceCore;
 
-type FrenchHornOscillatorPair<const P_FREQ: u32, const U_FREQ: u32> =
-    CoreOscillator<P_FREQ, U_FREQ, 10, 100, { OscillatorType::PulseWidth as usize }>;
-
-type FrenchHornOscillatorAdsr<const P_FREQ: u32, const U_FREQ: u32> =
-    CoreAdsr<P_FREQ, U_FREQ, 0, 3900, 96, 930, FrenchHornOscillatorPair<P_FREQ, U_FREQ>>;
-
-type FrenchHornFiltered<const P_FREQ: u32, const U_FREQ: u32> =
-    Filter<P_FREQ, U_FREQ, FrenchHornOscillatorAdsr<P_FREQ, U_FREQ>>;
-
-pub struct FrenchHorn<const P_FREQ: u32, const U_FREQ: u32> {
-    core: FrenchHornFiltered<P_FREQ, U_FREQ>,
-}
-
-impl<const P_FREQ: u32, const U_FREQ: u32> SoundSourceCore<P_FREQ, U_FREQ>
-    for FrenchHorn<P_FREQ, U_FREQ>
-{
-    type InitValuesType = SoundSourceNoteInit;
-
-    fn get_next(self: &mut Self) -> SoundSampleI32 {
-        self.core.get_next()
-    }
-
-    fn update(self: &mut Self) {
-        self.core.update()
-    }
-
-    fn has_next(self: &Self) -> bool {
-        self.core.has_next()
-    }
-
-    fn new(init_values: Self::InitValuesType) -> Self {
-        let frequency_1 = midi_note_to_freq(init_values.key);
-        let adsr_init = (init_values.velocity as i32) << 8;
-        let core = FrenchHornFiltered::<P_FREQ, U_FREQ>::new(((frequency_1, adsr_init), 800));
-        return Self { core };
-    }
-
-    fn trigger_note_off(self: &mut Self) {
-        self.core.trigger_note_off();
-    }
-
-    fn restart(self: &mut Self, vel: u8) {
-        self.core.restart(vel);
-    }
-}
+pub type FrenchHorn<const P_FREQ: u32, const U_FREQ: u32> = InstrumentTemplateAmpLfo<
+    P_FREQ,
+    U_FREQ,
+    30,                                      // Oscillator 0 pulse width
+    80,                                      // Oscillator 0 volume
+    { OscillatorType::PulseWidth as usize }, // Oscillator 0 wave form
+    0,                                       // Oscillator 0 note offset
+    5,                                       // Oscillator 1 pulse width
+    80,                                      // Oscillator 1 volume
+    { OscillatorType::PulseWidth as usize }, // Oscillator 1 wave form
+    0,                                       // Oscillator 1 note offset
+    false,                                   // Sync Oscillator 1 to 0
+    { OscillatorType::Sine as usize },       // LFO wave form
+    { 20 * FREQUENCY_MULTIPLIER / 2 },       // LFO frequency (10 hz)
+    10,                                      // LFO Depth
+    0,                                       // A
+    3900,                                    // D
+    96,                                      // S
+    300,                                     // R
+    GenericLowPassCalculator<90, 200>,
+>;
--- software/midi-nostd/src/instrument_low_pass_filters.rs
+++ software/midi-nostd/src/instrument_low_pass_filters.rs
@@ -24,6 +24,16 @@ impl FrequencyCalculator for PianoLowPassCalculator {
     }
 }
 
+pub struct GenericKeyBased<const KEY_SCALE: u32, const BASE: u32, const VEL_DIVIDE: u32> {}
+
+impl<const KEY_SCALE: u32, const BASE: u32, const VEL_DIVIDE: u32> FrequencyCalculator
+    for GenericKeyBased<KEY_SCALE, BASE, VEL_DIVIDE>
+{
+    fn get_cutoff_frequency(init_values: &SoundSourceNoteInit) -> u32 {
+        BASE + ((init_values.key as u32) * KEY_SCALE) + ((init_values.velocity as u32) / VEL_DIVIDE)
+    }
+}
+
 pub struct GenericLowPassCalculator<const SCALE_DOWN_PERCENT: u32, const OFFSET: u32> {}
 
 impl<const SCALE_DOWN_PERCENT: u32, const OFFSET: u32> FrequencyCalculator
