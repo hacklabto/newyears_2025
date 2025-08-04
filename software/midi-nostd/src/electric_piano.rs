7a19ae438385bbdc37785f4186e98ecde3ef38ac
Andrew Brownbill <andrew.brownbill@gmail.com>
2025-08-04 11:49:53 -0400

    Use new template for electric piano

--- software/midi-nostd/src/electric_piano.rs
+++ software/midi-nostd/src/electric_piano.rs
@@ -1,67 +1,32 @@
-use crate::adsr::CoreAdsr;
-use crate::double_oscillator::DoubleOscillator;
-use crate::filter::Filter;
-use crate::midi_notes::midi_note_to_freq;
+use crate::instrument_template_basic::FrequencyCalculator;
+use crate::instrument_template_basic::InstrumentTemplateBasic;
 use crate::note::SoundSourceNoteInit;
-use crate::oscillator::CoreOscillator;
 use crate::oscillator::OscillatorType;
-use crate::sound_sample::SoundSampleI32;
-use crate::sound_source_core::SoundSourceCore;
 
-type ElectricPianoOscillatorPair<const P_FREQ: u32, const U_FREQ: u32> = DoubleOscillator<
+pub struct ElectricPianoLowPassCalculator {}
+
+impl FrequencyCalculator for ElectricPianoLowPassCalculator {
+    fn get_cutoff_frequency(init_values: &SoundSourceNoteInit) -> u32 {
+        // experimentally derived
+        200 + ((init_values.key as u32) * 5) + ((init_values.velocity as u32) / 3)
+    }
+}
+
+pub type ElectricPiano<const P_FREQ: u32, const U_FREQ: u32> = InstrumentTemplateBasic<
     P_FREQ,
     U_FREQ,
-    CoreOscillator<P_FREQ, U_FREQ, 50, 100, { OscillatorType::SawTooth as usize }>,
-    CoreOscillator<P_FREQ, U_FREQ, 5, 100, { OscillatorType::PulseWidth as usize }>,
-    true,
+    50,                                      // Oscillator 0 pulse width
+    100,                                     // Oscillator 0 volume
+    { OscillatorType::SawTooth as usize },   // Oscillator 0 wave form
+    0,                                       // Oscillator 0 note offset
+    5,                                       // Oscillator 1 pulse width
+    100,                                     // Oscillator 1 volume
+    { OscillatorType::PulseWidth as usize }, // Oscillator 1 wave form
+    33,                                      // Oscillator 1 note offset
+    true,                                    // Sync Oscillator 1 to 0
+    0,                                       // A
+    5140,                                    // D
+    50,                                      // S
+    660,                                     // R
+    ElectricPianoLowPassCalculator,
 >;
-
-type ElectricPianoOscillatorAdsr<const P_FREQ: u32, const U_FREQ: u32> =
-    CoreAdsr<P_FREQ, U_FREQ, 0, 5140, 50, 660, ElectricPianoOscillatorPair<P_FREQ, U_FREQ>>;
-
-type ElectricPianoFiltered<const P_FREQ: u32, const U_FREQ: u32> =
-    Filter<P_FREQ, U_FREQ, ElectricPianoOscillatorAdsr<P_FREQ, U_FREQ>>;
-
-pub struct ElectricPiano<const P_FREQ: u32, const U_FREQ: u32> {
-    core: ElectricPianoFiltered<P_FREQ, U_FREQ>,
-}
-
-impl<const P_FREQ: u32, const U_FREQ: u32> SoundSourceCore<P_FREQ, U_FREQ>
-    for ElectricPiano<P_FREQ, U_FREQ>
-{
-    type InitValuesType = SoundSourceNoteInit;
-
-    fn get_next(self: &mut Self) -> SoundSampleI32 {
-        self.core.get_next()
-    }
-
-    fn update(self: &mut Self) {
-        self.core.update()
-    }
-
-    fn has_next(self: &Self) -> bool {
-        self.core.has_next()
-    }
-
-    fn new(init_values: Self::InitValuesType) -> Self {
-        let frequency_1 = midi_note_to_freq(init_values.key);
-        let frequency_2 = midi_note_to_freq(init_values.key + 24 + 9);
-        let cutoff_frequency =
-            200 + ((init_values.key as u32) * 4) + ((init_values.velocity as u32) / 2);
-
-        let adsr_init = (init_values.velocity as i32) << 8;
-        let core = ElectricPianoFiltered::<P_FREQ, U_FREQ>::new((
-            ((frequency_1, frequency_2), adsr_init),
-            cutoff_frequency,
-        ));
-        return Self { core };
-    }
-
-    fn trigger_note_off(self: &mut Self) {
-        self.core.trigger_note_off();
-    }
-
-    fn restart(self: &mut Self, vel: u8) {
-        self.core.restart(vel);
-    }
-}
