154445047bc83e8de57f86bb8b884f43d7951333
Andrew Brownbill <andrew.brownbill@gmail.com>
2025-08-09 19:50:10 -0400

    Finish splitting core0 and core1

--- software/rp2040_firmware/src/devices.rs
+++ software/rp2040_firmware/src/devices.rs
@@ -15,6 +15,8 @@ use embassy_rp::i2c;
 use embassy_rp::i2c::{SclPin, SdaPin};
 use embassy_rp::peripherals;
 use embassy_rp::peripherals::PIO0;
+use embassy_rp::peripherals::PIO1;
+use embassy_rp::pio::PioPin;
 use embassy_rp::Peripheral;
 use embassy_rp::Peripherals;
 use gpio::{Input, Pin, Pull};
@@ -24,10 +26,29 @@ use gpio::{Input, Pin, Pull};
 //
 pub struct Core0Resources<
     'a,
+    BackLightDma0: Peripheral + Channel,
+    BackLightData: PioPin,
+    BackLightClk: PioPin,
+    BackLightLatch: PioPin,
+    BackLightClr: PioPin,
+    BackLightTestData: Pin,
+    BackLightTestClk: Pin,
+    BackLightTestLatch: Pin,
+    BackLightTestClr: Pin,
     DisplayI2C: i2c::Instance,
     DisplayI2CScl: SclPin<DisplayI2C>,
     DisplayI2CSda: SdaPin<DisplayI2C>,
 > {
+    pub backlight_pio: PIO1,
+    pub backlight_dma0: BackLightDma0,
+    pub backlight_data: BackLightData,
+    pub backlight_clk: BackLightClk,
+    pub backlight_latch: BackLightLatch,
+    pub backlight_clr: BackLightClr,
+    pub backlight_test_data: BackLightTestData,
+    pub backlight_test_clk: BackLightTestClk,
+    pub backlight_test_latch: BackLightTestLatch,
+    pub backlight_test_clr: BackLightTestClr,
     pub button_back: Input<'a>,
     pub button_up: Input<'a>,
     pub button_down: Input<'a>,
@@ -39,12 +60,46 @@ pub struct Core0Resources<
 
 impl<
         'a,
+        BackLightDma0: Peripheral + Channel,
+        BackLightData: PioPin,
+        BackLightClk: PioPin,
+        BackLightLatch: PioPin,
+        BackLightClr: PioPin,
+        BackLightTestData: Pin,
+        BackLightTestClk: Pin,
+        BackLightTestLatch: Pin,
+        BackLightTestClr: Pin,
         DisplayI2C: i2c::Instance,
         DisplayI2CScl: SclPin<DisplayI2C>,
         DisplayI2CSda: SdaPin<DisplayI2C>,
-    > Core0Resources<'a, DisplayI2C, DisplayI2CScl, DisplayI2CSda>
+    >
+    Core0Resources<
+        'a,
+        BackLightDma0,
+        BackLightData,
+        BackLightClk,
+        BackLightLatch,
+        BackLightClr,
+        BackLightTestData,
+        BackLightTestClk,
+        BackLightTestLatch,
+        BackLightTestClr,
+        DisplayI2C,
+        DisplayI2CScl,
+        DisplayI2CSda,
+    >
 {
     pub fn new(
+        backlight_pio: PIO1,
+        backlight_dma0: BackLightDma0,
+        backlight_data: BackLightData,
+        backlight_clk: BackLightClk,
+        backlight_latch: BackLightLatch,
+        backlight_clr: BackLightClr,
+        backlight_test_data: BackLightTestData,
+        backlight_test_clk: BackLightTestClk,
+        backlight_test_latch: BackLightTestLatch,
+        backlight_test_clr: BackLightTestClr,
         button_back: impl Pin,
         button_up: impl Pin,
         button_down: impl Pin,
@@ -54,6 +109,16 @@ impl<
         display_i2c_sda: DisplayI2CSda,
     ) -> Self {
         Self {
+            backlight_pio,
+            backlight_dma0,
+            backlight_data,
+            backlight_clk,
+            backlight_latch,
+            backlight_clr,
+            backlight_test_data,
+            backlight_test_clk,
+            backlight_test_latch,
+            backlight_test_clr,
             button_back: Input::new(button_back, Pull::Up),
             button_up: Input::new(button_up, Pull::Up),
             button_down: Input::new(button_down, Pull::Up),
@@ -122,21 +187,32 @@ impl DevicesCore0<'_> {
         // let me do it one sub-system at a time
 
         let core0_resources = Core0Resources::new(
-            p.PIN_12, // Button, Back
-            p.PIN_14, // Button, Up
-            p.PIN_13, // Button, Down
-            p.PIN_15, // Button, Action
-            p.I2C0,   // Display I2C Interface
-            p.PIN_1,  // Display Scl
-            p.PIN_0,  // Display SDA
+            p.PIO1,    // Backlight PIO resource
+            p.DMA_CH0, // Backlight DMA channel
+            p.PIN_6,   // Backlight LED_CLK
+            p.PIN_7,   // Backlight LED_DATA
+            p.PIN_8,   // Backlight LED_LATCH
+            p.PIN_9,   // Backlight LED_CLEAR
+            p.PIN_22,  // Backlight LED_CLK
+            p.PIN_23,  // Backlight LED_DATA
+            p.PIN_24,  // Backlight LED_LATCH
+            p.PIN_25,  // Backlight LED_CLEAR
+            p.PIN_12,  // Button, Back
+            p.PIN_14,  // Button, Up
+            p.PIN_13,  // Button, Down
+            p.PIN_15,  // Button, Action
+            p.I2C0,    // Display I2C Interface
+            p.PIN_1,   // Display Scl
+            p.PIN_0,   // Display SDA
         );
 
         let core1_resources = Core1Resources::new(
-            //  Pio::new(p.PIO0, PioIrqs0),
-            p.PIO0, p.DMA_CH1, p.PIN_2,  // Sound A
-            p.PIN_3,  // Sound B.  Must be consequtive
-            p.PIN_4,  // ENA, always on
-            p.PIN_10, // Debug
+            p.PIO0,    // Assign PIO0 resource to Core 1
+            p.DMA_CH1, // Assign DMA Channel 1 to Core 1
+            p.PIN_2,   // Sound A Pin
+            p.PIN_3,   // Sound B Pin.  Must be consequtive
+            p.PIN_4,   // ENA Pin, always on
+            p.PIN_10,  // Debug Pin
         );
 
         let piosound = PioSound::new(
@@ -154,16 +230,16 @@ impl DevicesCore0<'_> {
                 max_row_pixels: 19,
                 num_intensity_levels: 255,
             },
-            p.PIO1,
-            p.PIN_6,  // LED_CLK
-            p.PIN_7,  // LED_DATA
-            p.PIN_8,  // LED_LATCH
-            p.PIN_9,  // LED_CLEAR
-            p.PIN_22, // LED_CLK
-            p.PIN_23, // LED_DATA
-            p.PIN_24, // LED_LATCH
-            p.PIN_25, // LED_CLEAR
-            p.DMA_CH0,
+            core0_resources.backlight_pio,
+            core0_resources.backlight_data,
+            core0_resources.backlight_clk,
+            core0_resources.backlight_latch,
+            core0_resources.backlight_clr,
+            core0_resources.backlight_test_data,
+            core0_resources.backlight_test_clk,
+            core0_resources.backlight_test_latch,
+            core0_resources.backlight_test_clr,
+            core0_resources.backlight_dma0,
         );
 
         Self {
