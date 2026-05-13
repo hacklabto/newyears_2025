23a22c5771a13bf7ed047b0f21078e7e74a444ec
Andrew Brownbill <andrew.brownbill@gmail.com>
2026-05-13 05:23:21 -0400

    Sample ends semi-gracefully when the song is over (there is a click)

--- software/example-midi/mapleleaf_rag/src/main.rs
+++ software/example-midi/mapleleaf_rag/src/main.rs
@@ -3,6 +3,8 @@
 use midi_nostd::midi::Midi;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use std::time::Duration;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Bring up midi player
@@ -30,24 +32,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sample_rate = config.sample_rate().0 as f32;
     let channels = config.channels() as usize;
 
+    // Make sure the sample rate is 44100, which is what I set the
+    // midi player to.  Knowing values like sample rate at compile
+    // time helps keep the size of the midi library small (10400
+    // bytes) and helps Rust optimize playback, but the cost is needing
+    // to know the playback rate in advance.
+    //
     println!("Playing on: {} Sample rate {}", device.name()?, sample_rate);
-
-    let mut next_value = move || {
-
-        let current = (midi.get_next().clip().to_i32() as f32) / 32768.0;
-        current
-    };
+    assert!( sample_rate == 44100.0 );
 
     // 5. Build the output stream
     let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
-    
+ 
+    // Shared state: 'false' means still playing, 'true' means source exhausted
+    let finished = Arc::new(AtomicBool::new(false));
+    let finished_for_callback = finished.clone();
+
     let stream = device.build_output_stream(
         &config.into(),
         move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
             for frame in data.chunks_mut(channels) {
-                let value = next_value();
+                let value = (midi.get_next().clip().to_i32() as f32) / 32768.0;
                 for sample in frame.iter_mut() {
                     *sample = value;
+                }
+                if !midi.has_next() {
+                    finished_for_callback.store(true, Ordering::Relaxed);
                 }
             }
         },
@@ -57,7 +67,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // 6. Start the stream and wait
     stream.play()?;
-    std::thread::sleep(Duration::from_millis(20000));
+    while !finished.load(Ordering::Relaxed) {
+        std::thread::sleep(Duration::from_millis(100));
+    }
+    stream.pause()?;
 
     Ok(())
 }
